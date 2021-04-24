fe2f7ebc97ac3ef58a5119ebdc6c579c3193a2cb
MatuszPason <mateuszpason64@gmail.com>
2021-04-24 17:09:40 +0200

    Name change, file will contain more classes with interfaces that inform about successful actions

--- Application/SuccessfulNewEntryInDatabaseInfo.py
+++ Application/SuccessfulNewEntryInDatabaseInfo.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog
 from PyQt5.uic import loadUi
 import loginOrRegisterForm
+import PersonAccountView
 
 
 class SuccessfulRegister(QDialog):
@@ -14,3 +15,14 @@ class SuccessfulRegister(QDialog):
         self.close()
         loginOrRegisterForm.OpeningWindow()
 
+
+class SuccessfulNewPort(QDialog):
+    def __init__(self):
+        super(SuccessfulNewPort, self).__init__()
+        self.ui = loadUi("Resources/interfaces/Successful_port_add.ui", self)
+        self.show()
+        self.go_back.clicked.connect(self.to_CEO_panel)
+
+    def to_CEO_panel(self):
+        self.close()
+        PersonAccountView.CEOAccountView()
